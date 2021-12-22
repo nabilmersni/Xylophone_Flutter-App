601ec60f8b561684a692a2d4d66bb21f64da413a
nabilmersni <nabilmersni123@gmail.com>
2021-12-22 17:30:20 +0100

    finishing the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,17 +13,57 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: AppBody(),
       ),
     );
   }
 }
 
-class AppBody extends StatelessWidget {
+class AppBody extends StatefulWidget {
   const AppBody({Key? key}) : super(key: key);
 
   @override
+  _AppBodyState createState() => _AppBodyState();
+}
+
+class _AppBodyState extends State<AppBody> {
+  final audioPlayer = AudioPlayer();
+
+  void playSound(notePath) async {
+    await audioPlayer.play(notePath, isLocal: true);
+  }
+
+  Expanded buidKey({color, notePath}) {
+    return Expanded(
+      flex: 1,
+      child: TextButton(
+        onPressed: () {
+          playSound(notePath);
+        },
+        child: Container(
+          width: double.infinity,
+          color: color,
+        ),
+        style: TextButton.styleFrom(
+          padding: EdgeInsets.zero,
+        ),
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Column();
+    return Column(
+      children: [
+        buidKey(color: Color(0xffF53832), notePath: 'assets/note1.wav'),
+        buidKey(color: Color(0xffFF8A00), notePath: 'assets/note2.wav'),
+        buidKey(color: Color(0xffFFE63C), notePath: 'assets/note3.wav'),
+        buidKey(color: Color(0xff44A44B), notePath: 'assets/note4.wav'),
+        buidKey(color: Color(0xff068175), notePath: 'assets/note5.wav'),
+        buidKey(color: Color(0xff1F77D3), notePath: 'assets/note6.wav'),
+        buidKey(color: Color(0xff771B88), notePath: 'assets/note7.wav'),
+      ],
+    );
   }
 }
